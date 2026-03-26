986f31500927ae84ad7474b9cc97eb17d5f18f8f
Amr Turki <amrturki79@gmail.com>
2026-03-26 17:53:37 +0200

    finish menu

--- Project #2 hospital/hospital.cpp
+++ Project #2 hospital/hospital.cpp
@@ -2,7 +2,28 @@
 using namespace std;
 
 
+int menu() {
+    int choice = -1;
+    while(choice == -1)
+    {
+        cout << "Enter your choice:\n";
+        cout << "1) Add new patient\n";
+        cout << "2) Print all patients\n";
+        cout << "3) Get next patient\n";
+        cout << "4) Exit\n";
 
+        cin>>choice;
+
+        if (!(choice>=1 && choice<=4)) 
+        {
+            cout<<"Invalid choice\n";
+            cout<<'Again\n';
+
+            choice = -1;
+        }
+    }
+    return choice;
+}
 int main()
 {
 
