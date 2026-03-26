bdd204450e970a364dda45bc8bb613917390528e
Amr Turki <amrturki79@gmail.com>
2026-03-26 17:57:10 +0200

    implement main menu loop and program flow logic

--- Project #2 hospital/hospital.cpp
+++ Project #2 hospital/hospital.cpp
@@ -1,7 +1,17 @@
 #include<iostream>
 using namespace std;
+bool AddPatient()
+{
+    
+}
+void PrintAllPatients()
+{
 
+}
+bool GetNextPatient()
+{
 
+}
 int menu() {
     int choice = -1;
     while(choice == -1)
@@ -24,8 +34,27 @@ int menu() {
     }
     return choice;
 }
+void hospital_program() {
+
+    while (true) {
+        int choice = menu();
+
+        if (choice == 1) {
+            AddPatient();
+        }
+        else if (choice == 2) {
+            PrintAllPatients();
+        }
+        else if (choice == 3) {
+            GetNextPatient();
+        }
+        else if(choice == 4){
+            break;
+        }
+    }
+}
 int main()
 {
-
+    hospital_program();
     return 0;
 }
