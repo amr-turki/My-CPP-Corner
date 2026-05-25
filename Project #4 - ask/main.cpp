003c70d650d30ad2e096ccb7ebede3ca69689d60
Amr Turki <amrturki79@gmail.com>
2026-05-25 10:15:00 +0300

    Create Ask Project

---
+++ Project #4 - ask/main.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+class AskMe
+{
+    private:
+  
+public:
+    
+    void run()
+    {
+
+    }
+};
+int main()
+{
+    AskMe askMe;
+    askMe.run();
+
+
+    return 0;
+}
