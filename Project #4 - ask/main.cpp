a20478920efc0ab9c6d5648595bb67c5cd7140ee
Amr Turki <amrturki79@gmail.com>
2026-06-08 10:42:00 +0300

    edit in login and signup

--- Project #4 - ask/main.cpp
+++ Project #4 - ask/main.cpp
@@ -12,16 +12,18 @@ class USERS {
     string name;
     string email;
     string allow_anonymous_questions;
+    static int inline id = 150;
+    string user_id;
 public:
-    bool signUp() {
+    string signUp() {
         auto status =  ios::in | ios::out | ios::app;
         fstream file_handler("users.txt",status);
         if (file_handler.fail()) {
-            cout<<"We can not open file\n";
-            return false;
+            return "We can not open file\n";
         }
 
-
+        user_id = to_string(++id);
+        file_handler<<user_id<<",";
         std::cout << "Enter user name. (No spaces): ";
         getline(cin,user_name);
         file_handler<<user_name<<",";
@@ -44,13 +46,12 @@ public:
         file_handler.close();
 
 
-        return true;
+        return user_id;
     }
-    bool login() {
+    string login() {
        ifstream file_handler("users.txt");
         if (file_handler.fail()) {
-            cout<<"We can not open file\n";
-            return false;
+            return "We can not open file\n";
         }
 
         cout<<"Enter user name & password\n";
@@ -65,15 +66,16 @@ public:
             while (getline(in,text,',')) {
                 results.push_back(text);
             }
-            if (results[0] == user_name && results[1] == password) {
-                return true;
+            if (results[1] == user_name && results[2] == password) {
+                return user_id;
             }
         }
         file_handler.close();
 
-        return false;
+        return "User is not found";
     }
 };
+
 
 class AskMe
 {
@@ -142,41 +144,57 @@ public:
         cin >> choice;
         cin.ignore();
 
-        bool status = true;
+        string user_id;
         if (choice == 1) {
-           status = user.login();
-        }else {
-            status = user.signUp();
-        }
-        
-        if (status) {
-            while (true) {
-                int choice = menu();
+            if ( user.login() == "We can not open file\n")
+            {
+                cout<<"We can not open file\n";
+                return;
+            }
 
-                if (choice == 1) {
-                    printQuestionsToMe();
-                }
-                else if (choice == 2) {
-                    printQuestionsFromMe();
-                }
-                else if (choice == 3) {
-                    answerQuestion();
-                }
-                else if (choice == 4) {
-                    deleteQuestion();
-                }
-                else if (choice == 5) {
-                    askQuestion();
-                }
-                else if (choice == 6) {
-                    listSystemUsers();
-                }
-                else if (choice == 7) {
-                    feed();
-                }
-                else if (choice == 8) {
-                    logout();
-                }
+            else if (user.login() == "User is not found") {
+                cout<<"User is not found\n";
+                return;
+            }
+            else {
+                user_id = user.login();
+            }
+
+        }else {
+            if (user.signUp() == "We can not open file\n") {
+                cout<<"We can not open file\n";
+            }
+            else
+                user_id = user.signUp();
+        }
+
+        while (true) {
+            int choice = menu();
+
+            if (choice == 1) {
+                printQuestionsToMe();
+            }
+            else if (choice == 2) {
+                printQuestionsFromMe();
+            }
+            else if (choice == 3) {
+                answerQuestion();
+            }
+            else if (choice == 4) {
+                deleteQuestion();
+            }
+            else if (choice == 5) {
+                //askQuestion(user_id);
+            }
+            else if (choice == 6) {
+                listSystemUsers();
+            }
+            else if (choice == 7) {
+                feed();
+            }
+            else if (choice == 8) {
+                break;
+                //logout();
             }
         }
 
